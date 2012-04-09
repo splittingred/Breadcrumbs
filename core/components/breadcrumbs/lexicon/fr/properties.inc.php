7f6b3dbfda97110e16c7f8725223409c9398fb8c
Jerome Perrin <hello@jeromeperrin.com>
2012-04-09 23:01:39 +0200

    Polishing French translation + Cleaning up unused keys in all languages

--- core/components/breadcrumbs/lexicon/fr/properties.inc.php
+++ core/components/breadcrumbs/lexicon/fr/properties.inc.php
@@ -28,24 +28,22 @@
  * @subpackage lexicon
  * @language fr
  */
-$_lang['breadcrumbs.prop_desc.maxCrumbs'] = 'Nombre maximun de liens dans le fil d\'ariane. Si vous réduisez le nombre à, disons 5, et que la page vue est à 5 niveaux de profondeur, le fil d\'ariane sera : Accueil > ... > Niveau 4 > Niveau 5. "Accueil" et la page courante ne sont pas comptés et sont configurés séparément..';
+$_lang['breadcrumbs.prop_desc.maxCrumbs'] = 'Nombre maximun de liens dans le fil d\'ariane. Si vous réduisez le nombre à, disons 5, et que la page vue est au 5ème niveaux de profondeur, le fil d\'ariane sera : Accueil > ... > Niveau 4 > Niveau 5. "Accueil" et la page courante ne sont pas comptés et sont configurés séparément.';
 $_lang['breadcrumbs.prop_desc.pathThruUnPub'] = 'Quand le chemin contient un dossier non-publié, mettre cette propriété à 1 montrera toutes les ressources du chemin SAUF celles qui ne sont pas publiés. Voir la documentation pour plus de détails : http://rtfm.modx.com/display/ADDON/Breadcrumbs';
-$_lang['breadcrumbs.prop_desc.respectHidemenu'] = 'Masquer les ressources qui sont masqués dans les menu?';
-$_lang['breadcrumbs.prop_desc.showHomeCrumb'] = 'Afficher la page d\'accueil dans le fil d\'ariane ? Ceci n\'est pas toujours nécessaire car un lien vers l\'accueil est habituellement disponible dans la navigation ou en cliquant sur le logo du site.';
+$_lang['breadcrumbs.prop_desc.respectHidemenu'] = 'Masquer les ressources qui sont masqués dans les menus ?';
+$_lang['breadcrumbs.prop_desc.showHomeCrumb'] = 'Afficher un lien vers la page d\'accueil dans le fil d\'ariane ? Ceci n\'est pas toujours nécessaire car ce lien est habituellement disponible dans la navigation ou en cliquant sur le logo du site.';
 $_lang['breadcrumbs.prop_desc.showCrumbsAtHome'] = 'Montrer le fil d\'ariane sur la page d\'accueil ?';
 $_lang['breadcrumbs.prop_desc.showCurrentCrumb'] = 'Inclure la page courante ?';
 $_lang['breadcrumbs.prop_desc.currentAsLink'] = 'Nom de la page courante cliquable ?';
-$_lang['breadcrumbs.prop_desc.crumbSeparator'] = 'Séparateur';
+$_lang['breadcrumbs.prop_desc.crumbSeparator'] = 'Caractère de séparation';
 $_lang['breadcrumbs.prop_desc.homeCrumbTitle'] = 'Nom de la page d\'accueil dans le fil d\'ariane.';
 $_lang['breadcrumbs.prop_desc.homeCrumbDescription'] = 'Description du lien de la page d\'accueil.';
-$_lang['breadcrumbs.prop_desc.titleField'] = 'Champs de la ressource à utiliser pour le titre des liens. Valeur par défaut : pagetitle.';
-$_lang['breadcrumbs.prop_desc.descField'] = 'Champs de la ressource à utiliser pour la description des liens. Valeur par défaut : description (si vide, pagetitle sera utilisé).';
+$_lang['breadcrumbs.prop_desc.titleField'] = 'Champ de la ressource à utiliser pour le titre des liens. Valeur par défaut : pagetitle.';
+$_lang['breadcrumbs.prop_desc.descField'] = 'Champ de la ressource à utiliser pour la description des liens. Valeur par défaut : description (si vide, pagetitle sera utilisé).';
 $_lang['breadcrumbs.prop_desc.maxDelimiter'] = 'Le caractère à utiliser si le nombre maximum de liens est atteint.';
 $_lang['breadcrumbs.prop_desc.bcTplCrumbCurrent'] = '';
 $_lang['breadcrumbs.prop_desc.bcTplCrumbCurrentLink'] = '';
-$_lang['breadcrumbs.prop_desc.bcTplCrumbFirst'] = '';
 $_lang['breadcrumbs.prop_desc.bcTplCrumbHome'] = '';
-$_lang['breadcrumbs.prop_desc.bcTplCrumbLast'] = '';
 $_lang['breadcrumbs.prop_desc.bcTplCrumbMax'] = '';
 $_lang['breadcrumbs.prop_desc.bcTplCrumbLink'] = '';
 $_lang['breadcrumbs.prop_desc.bcTplCrumbOuter'] = '';
